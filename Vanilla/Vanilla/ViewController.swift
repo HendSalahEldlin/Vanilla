1a2c95b338d2a091d06176dfc8a96f68bd7358c0
Hend <h.salaheldin95@gmail.com>
2019-08-26 09:10:15 -0700

    Create the Project

---
+++ Vanilla/Vanilla/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vanilla
+//
+//  Created by Hend  on 8/26/19.
+//  Copyright © 2019 Hend . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
