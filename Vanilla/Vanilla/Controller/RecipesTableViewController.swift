739dab29ea69cbc95a53b6d420c0f45922e92714
Hend <h.salaheldin95@gmail.com>
2019-08-28 17:02:43 -0700

    Updated the table view design and added the share and fav btns

--- Vanilla/Vanilla/Controller/RecipesTableViewController.swift
+++ Vanilla/Vanilla/Controller/RecipesTableViewController.swift
@@ -15,6 +15,8 @@ class RecipesTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.register(TVRecipeCell.self, forCellReuseIdentifier: "TVRecipeCell")
         spoonacular.sharedInstance().getRecipes() {(success, error) in
             DispatchQueue.main.async {
                 if success{
@@ -37,22 +39,68 @@ class RecipesTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TVRecipeCell")! as! TVRecipeCell
+        
+        cell.delegate = self
         let recipe = self.recipes[(indexPath as NSIndexPath).row]
         
         // Set the name and image
         cell.titleLabel.text = recipe.title
-        cell.readyInMinutesLabel.text = "\(recipe.readyInMinutes ?? 0)"
-        if let image = recipe.image{
-            let url = URL(string: spoonacular.Constants.baseUri + image)!
-            let data = try? Data(contentsOf: url)
-            
-            if let imageData = data {
-                cell.imageView?.image = UIImage(data: imageData)
-            }
-        }
+        cell.minutesLabel.text = "\(recipe.readyInMinutes ?? 0) minutes"
         
+        if let imageData = getImage(indexPath: indexPath) {
+            cell.imageView?.image = UIImage(data: imageData)
+        }
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let recipe = self.recipes[(indexPath as NSIndexPath).row]
+        print(recipe.title!)
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let imageData = getImage(indexPath: indexPath) else{
+            return 0
+        }
+        let imageRatio = UIImage(data: imageData)!.getImageRatio()
+        return tableView.frame.width / imageRatio
+    }
+    
+    func getImage(indexPath : IndexPath) -> Data? {
+        let recipe = self.recipes[(indexPath as NSIndexPath).row]
+        guard let image = recipe.image else{
+            return nil
+        }
+        let url = URL(string: spoonacular.Constants.baseUri + image)!
+        let data = try? Data(contentsOf: url)
+        return data
+    }
+    
+    
+    /*@objc func favBtnTouched(_ sender: UIButton){
+        print("favBtnTouched")
+        /*let cell = tableView.dequeueReusableCell(withIdentifier: "TVRecipeCell", for: indexPath) as! TVRecipeCell
+        let image = UIImage(named: "redHeart-30x30") as UIImage?
+        cell.favBtn.setImage(image, for: .normal)*/
+    }*/
+}
+
+extension UIImage {
+    func getImageRatio() -> CGFloat {
+        let imageRatio = CGFloat(self.size.width / self.size.height)
+        return imageRatio
     }
 }
 
 
+extension RecipesTableViewController: CellActionDelegate {
+    func shareARecipe() {
+        print("share")
+    }
+    
+    func addToFav() {
+        print("favourite")
+    }
+    
+    
+}
--- Vanilla/Vanilla/Controller/TVRecipeCell.swift
+++ Vanilla/Vanilla/Controller/TVRecipeCell.swift
@@ -8,8 +8,98 @@
 
 import Foundation
 import UIKit
+
+
+protocol CellActionDelegate {
+    func shareARecipe()
+    func addToFav()
+}
+
+
 class TVRecipeCell: UITableViewCell {
     
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var readyInMinutesLabel: UILabel!
+    var delegate: CellActionDelegate?
+    
+     var mainImageView : UIImageView  = {
+     var imageView = UIImageView(frame: CGRect.init(x: 0, y: 0, width: 0, height: 0))
+     imageView.translatesAutoresizingMaskIntoConstraints = false
+     imageView.clipsToBounds = true
+     return imageView
+     }()
+    
+    var titleLabel : UILabel = {
+        var label = UILabel(frame: CGRect.init(x: 0, y: 0, width: 0, height: 0))
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor.red
+        return label
+    }()
+    
+    var minutesLabel : UILabel = {
+        var label = UILabel(frame: CGRect.init(x: 0, y: 0, width: 0, height: 0))
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor.red
+        return label
+    }()
+    
+    var shareBtn : UIButton = {
+        let button   = UIButton(type: .custom) as UIButton
+        button.translatesAutoresizingMaskIntoConstraints = false
+        let image = UIImage(named: "share_30x30") as UIImage?
+        button.setImage(image, for: .normal)
+        return button
+    }()
+    
+    
+    
+    var favBtn : UIButton = {
+        let button  = UIButton(type: .custom) as UIButton
+        button.translatesAutoresizingMaskIntoConstraints = false
+        let image = UIImage(named: "whiteHeart-30x30") as UIImage?
+        button.setImage(image, for: .normal)
+        //button.addTarget(self, action: #selector(favBtnTouched(_:)), for:.touchUpInside)
+        return button
+    }()
+     
+     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.backgroundColor = UIColor.black
+        self.addSubview(mainImageView)
+        mainImageView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        mainImageView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -20).isActive = true
+        mainImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 20).isActive = true
+        mainImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -20).isActive = true
+        
+        mainImageView.addSubview(titleLabel)
+        titleLabel.leftAnchor.constraint(equalTo: self.mainImageView.leftAnchor, constant: 30).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: self.mainImageView.topAnchor, constant: 20).isActive = true
+        
+        
+        mainImageView.addSubview(minutesLabel)
+        minutesLabel.leftAnchor.constraint(equalTo: self.mainImageView.leftAnchor, constant: 30).isActive = true
+        minutesLabel.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 20).isActive = true
+        
+        self.addSubview(shareBtn)
+        shareBtn.rightAnchor.constraint(equalTo: self.mainImageView.rightAnchor, constant: -30).isActive = true
+        shareBtn.bottomAnchor.constraint(equalTo: self.mainImageView.bottomAnchor, constant: -20).isActive = true
+        shareBtn.addTarget(self, action: #selector(share), for:.touchUpInside)
+        
+        self.addSubview(favBtn)
+        favBtn.rightAnchor.constraint(equalTo: self.shareBtn.leftAnchor, constant: -15).isActive = true
+        favBtn.bottomAnchor.constraint(equalTo: self.mainImageView.bottomAnchor, constant: -20).isActive = true
+        favBtn.addTarget(self, action: #selector(favourite), for:.touchUpInside)
+        
+     }
+     required init?(coder aDecoder: NSCoder) {
+     fatalError("init(coder:) has not been implemented")
+     }
+    
+    @objc func share(sender: UIButton!){
+        delegate?.shareARecipe()
+    }
+    
+    @objc func favourite(){
+        delegate?.addToFav()
+    }
+    
 }
